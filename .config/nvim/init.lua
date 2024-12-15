d75df4a7672fb820f6e7ed0f4f36eec749da04b8
Matt Maloney <matttmaloney@gmail.com>
2024-12-14 21:51:07 -0500

    refactoring colorschemes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -733,69 +733,7 @@ require("lazy").setup({
 			})
 		end,
 	},
-
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-		--  "nyoom-engineering/oxocarbon.nvim",
-		--  'folke/tokyonight.nvim',
-		"maxmx03/fluoromachine.nvim",
-		config = function()
-			local fm = require("fluoromachine")
-
-			fm.setup({
-				glow = true,
-				theme = "fluoromachine",
-			})
-
-			vim.cmd.colorscheme("fluoromachine")
-		end,
-		--  "catppuccin/nvim",
-		--  name = "catppuccin",
-		--  config = function()
-		--  	require("catppuccin").setup({
-		--  		no_italic = true,
-		--  		term_colors = true,
-		--  		transparent_background = false,
-		--  		styles = {
-		--  			comments = {},
-		--  			conditionals = {},
-		--  			loops = {},
-		--  			functions = {},
-		--  			keywords = {},
-		--  			strings = {},
-		--  			variables = {},
-		--  			numbers = {},
-		--  			booleans = {},
-		--  			properties = {},
-		--  			types = {},
-		--  		},
-		--  		color_overrides = {
-		--  			mocha = {
-		--  				base = "#000000",
-		--  				mantle = "#000000",
-		--  				crust = "#000000",
-		--  			},
-		--  		},
-		--  		integrations = {
-		--  			telescope = {
-		--  				enabled = true,
-		--  				style = "nvchad",
-		--  			},
-		--  			dropbar = {
-		--  				enabled = true,
-		--  				color_mode = true,
-		--  			},
-		--  		},
-		--  	})
-
-		--  	-- setup must be called before loading
-		--  	vim.cmd.colorscheme("catppuccin")
-		--  end,
-		priority = 1000,
-	},
+	require("custom.colorschemes.cyberdream"),
 
 	-- Highlight todo, notes, etc in comments
 	{
@@ -889,7 +827,7 @@ require("lazy").setup({
 	-- require 'kickstart.plugins.autopairs',
 	-- require 'kickstart.plugins.neo-tree',
 	-- require("kickstart.plugins.gitsigns"), -- adds gitsigns recommend keymaps
-	require("kickstart.plugins.hlchunk"), -- adrequire("kickstart.plugins.hlchunk"), -- adds gitsigns recommend keymaps
+	require("kickstart.plugins.hlchunk"), -- adds color lines to respective braces
 	require("kickstart.plugins.lualine.lualine"), -- adds gitsigns recommend keymaps
 	require("kickstart.plugins.oil"),
 	-- {
